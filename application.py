750bfbb59af90a401c8df50142f995619cc17746
Max Mumford <maxmumford@gmail.com>
2014-03-18 12:20:07 +0100

    [FIX] amazon could not find application

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from app import app
+from app import app as application
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    application.run(host='0.0.0.0', debug=True)
 
