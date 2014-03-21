b8f601a35c77327b0f83c33172ecf0db00caefe3
Max <max@max-ubuntu.(none)>
2014-03-21 23:07:09 +0100

    [ADD] social media buttons [ADD] half white page tempate [ADD] media page example content

--- app/__init__.py
+++ app/__init__.py
@@ -9,5 +9,5 @@ app.config.update(
     SECRET_KEY='\xeeJ\x97\xe4\x9f\x96\xe6\xc3\xe7:\xca\xc9L}F\xb3\xe6\x84\xfa\xaf\x8d\x14\x83\xa4'
 )
 
-# import views
+# import custom python files
 from app import views
