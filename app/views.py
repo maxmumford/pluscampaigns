4ad9cb6d48e6cf4eae7936d073f61cc8ec36e001
Max Mumford <maxmumford@gmail.com>
2014-03-23 18:57:54 +0100

    [ADD] all pages use text-full template [ADD] enlarge main title

--- app/views.py
+++ app/views.py
@@ -7,16 +7,16 @@ def index():
 
 @app.route('/campaign')
 def campaign():
-    return render_template('campaign.html', title="Your Campaign", heading="Your Campaign", image_source="4")
+    return render_template('campaign.html', title="Your Campaign", heading="Your Campaign", image_source="6")
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title="Young Voters", heading="Vote In May", image_source="1")
+    return render_template('about.html', title="Young Voters", heading="Vote In May", image_source="7")
 
 @app.route('/take-action')
 def share():
-    return render_template('take-action.html', title="Take Action", heading="Take Action", image_source="3")
+    return render_template('take-action.html', title="Take Action", heading=False, image_source="2")
 
 @app.route('/media')
 def media():
-    return render_template('media.html', title="Media", heading="Media Resources", image_source="2")
+    return render_template('media.html', title="Media", heading="Media Resources", image_source="1")
