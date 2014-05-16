14b699a30b93b27f0e7eef4843c59e37ef24be15
Max Mumford <maxmumford@gmail.com>
2014-05-16 15:06:22 +0200

    [ADD] fading background images and css [ADD] side bar menu [ADD] shadows and general page styling [IMP] styling on text pages

--- app/jinja_functions.py
+++ app/jinja_functions.py
@@ -9,7 +9,7 @@ def menu_active(path):
 		return ''
 
 def random_background_image():
-	images = ['1.jpg', '2.jpg', '3.png', '4.JPG', '5.jpg', '6.jpg', '7.jpg', '8.png', '9.jpg', '10.jpg', '11.jpg']
+	images = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png', '7.png', '8.png', '9.png', '10.png', '11.png']
 	return images[random.randint(0, len(images)-1)]
 
 app.jinja_env.globals.update(menu_active=menu_active)
--- app/views.py
+++ app/views.py
@@ -20,7 +20,7 @@ def media():
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', title="Contact", heading="Contact Us")
+    return render_template('contact.html', title="Contact")
 
 @app.route('/your-campaign')
 def campaign():
@@ -29,3 +29,20 @@ def campaign():
 @app.route('/research')
 def research():
     return render_template('research.html', title="We're in Touch", heading="We're in Touch")
+
+# side bar menu
+@app.route('/audio-visual')
+def audio_visual():
+    return render_template('audio-visual.html', title="Audio Visual", heading="Audio Visual")
+
+@app.route('/social-media')
+def social_media():
+    return render_template('social-media.html', title="Social Media", heading="Social Media")
+
+@app.route('/events')
+def events():
+    return render_template('events.html', title="Events & Organisation", heading="Events & Organisation")
+
+@app.route('/editorial')
+def editorial():
+    return render_template('editorial.html', title="Editorial & New Content", heading="Editorial & New Content")
